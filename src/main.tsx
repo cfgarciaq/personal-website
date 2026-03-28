c2f9a489c3a3814e34372f49a2cfcdbd0391dcf0
cfgarciaq <cfgarciaq@gmail.com>
2026-03-28 20:25:11 +0100

    feat(i18n): setup i18n structure and configuration with EN/ES/FR support

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/main.css'
 import App from './App.tsx'
+import './i18n/config'
 
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
