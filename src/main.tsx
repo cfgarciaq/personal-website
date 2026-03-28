5d5ce3cba59f4ee70213e3cfdc5d4442d0f40139
cfgarciaq <cfgarciaq@gmail.com>
2026-03-28 19:40:24 +0100

    fix/corrected import of SpeedInsight to use the correct format for React after Vercel failed to build

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './styles/main.css'
 import App from './App.tsx'
 
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
